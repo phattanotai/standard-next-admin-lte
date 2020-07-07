5e18f42a16b6cad1dd5e047d182be5ce68fdf571
svdevtech <svdevtech@gmail.com>
2020-07-08 03:53:05 +0700

    change message next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withSass = require("@zeit/next-sass")
 
-module.exports = withSass()
+module.exports = withSass() 
 
 const path = require('path')
 
@@ -8,4 +8,25 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-}
+  devIndicators: {
+    autoPrerender: false,
+  },
+  module: {
+    rules: [
+      {
+        // Transpiles ES6-8 into ES5
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        // Loads the javacript into html template provided.
+        // Entry point is set below in HtmlWebPackPlugin in Plugins
+        test: /\.html$/,
+        use: [{loader: "html-loader"}]
+      }
+    ]
+  },
+} 
