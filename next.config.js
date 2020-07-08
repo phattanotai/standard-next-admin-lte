0a34df633b125a3e2fcb922d8097a806187da2ae
svdevtech <svdevtech@gmail.com>
2020-07-08 09:12:54 +0700

    edit package json

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withSass = require("@zeit/next-sass")
 
 module.exports = withSass() 
 
-const path = require('path')
+/* const path = require('path')
 
 module.exports = {
   sassOptions: {
@@ -29,4 +29,4 @@ module.exports = {
       }
     ]
   },
-} 
+}  */
